05b9e510f12901096541e39b825b7fb5775e9518
Jolan Aklin <jolan.aklin@rpn.ch>
2021-04-27 14:13:08 +0200

    Ajoutez des fichiers projet.

---
+++ ProgTheRobotSetup/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ProgTheRobotSetup
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            DownloadGrid.Visibility = Visibility.Hidden;
+        }
+
+        private void StartInstall(object sender, RoutedEventArgs e)
+        {
+            MainGrid.Visibility = Visibility.Hidden;
+            DownloadGrid.Visibility = Visibility.Visible;
+
+            string path = $@"C:\Users\{Environment.UserName}\Desktop\Prog_the_robot_v1-0a.zip";
+            WebClient webClient = new WebClient();
+            webClient.DownloadProgressChanged += onProgressChanged;
+            webClient.DownloadFileAsync(new Uri("https://github.com/JolanAklin/ProgTheRobot/releases/download/v1.0a/Prog_the_robot_v1-0a.zip"), path);
+        }
+
+        private void onProgressChanged(object sender, DownloadProgressChangedEventArgs e)
+        {
+            DownLoadProgress.Value = e.ProgressPercentage;
+        }
+    }
+}
